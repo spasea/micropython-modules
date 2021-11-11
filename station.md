7d7fef1087a7b332210b7de76a71f269ae23b3e6
ithespv <ithespv@gmail.com>
2021-11-11 19:15:36 +0200

    Add single string station to connect

--- station.md
+++ station.md
@@ -7,3 +7,13 @@ from station import Station
 wlan = Station("ssid", "password")
 print('http://' + wlan.station.ifconfig()[0])
 ```
+
+### Also the list of networks can be passed
+
+```python
+from station import Station
+
+
+wlan = Station(single_string="ssid__password____ssid1__password1")
+print('http://' + wlan.station.ifconfig()[0])
+```
--- station.py
+++ station.py
@@ -2,10 +2,52 @@ import network
 
 
 class Station:
-    def __init__(self, ssid, password):
+    def __init__(self, ssid='', password='', single_string=''):
+        self.station = ''
+
+        if single_string != '':
+            self.single_string = single_string
+            self.find()
+
+            return
+
         self.password = password
         self.ssid = ssid
-        self.station = ''
+
+        self.connect()
+
+    def find(self):
+        station = network.WLAN(network.STA_IF)
+        station.active(True)
+        stations = station.scan()
+        stations_dict = {}
+
+        for station_item in stations:
+            stations_dict[station_item[0].decode()] = ''
+
+        config_stations = self.single_string.split('____')
+        station_to_connect = ''
+
+        for config_station in config_stations:
+            if station_to_connect != '':
+                break
+
+            [ssid, p] = config_station.split('__')
+
+            if ssid not in stations_dict:
+                continue
+
+            station_to_connect = config_station
+
+        if station_to_connect == '':
+            raise Exception('No available station')
+
+            return
+
+        [ssid, password] = station_to_connect.split('__')
+
+        self.password = password
+        self.ssid = ssid
 
         self.connect()
 
